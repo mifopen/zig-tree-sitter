a3bb216c29b995d7dea5ced42ab441a6ba251063
Slava Mostovoy <mif.open@gmail.com>
2023-07-18 21:40:56 +0200

    prepared as a dependency

--- build.zig
+++ build.zig
@@ -4,34 +4,40 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
+    _ = b.addModule("zig-tree-sitter", .{
+        .source_file = .{ .path = "src/main.zig" },
+    });
+
     const lib = b.addStaticLibrary(.{
         .name = "zig-tree-sitter",
         .target = target,
         .optimize = optimize,
     });
-
     lib.addCSourceFile("upstream/lib/src/lib.c", &.{});
     lib.addIncludePath("upstream/lib/src");
     lib.addIncludePath("upstream/lib/include");
+    b.installArtifact(lib);
 
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = .{ .path = "src/tests.zig" },
         .target = target,
         .optimize = optimize,
     });
-
-    main_tests.linkLibrary(lib);
-    main_tests.addIncludePath("upstream/lib/src");
-    main_tests.addIncludePath("upstream/lib/include");
+    linkTreeSitter(main_tests);
     main_tests.addCSourceFiles(&[_][]const u8{
         "src/json_parser.c",
         "src/typescript_parser.c",
         "src/typescript_scanner.c",
         "src/typescript_scanner.h",
     }, &.{});
-
     const run_main_tests = b.addRunArtifact(main_tests);
-
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
+}
+
+fn linkTreeSitter(step: *std.build.Step.Compile) void {
+    step.linkLibC();
+    step.addCSourceFile("upstream/lib/src/lib.c", &.{});
+    step.addIncludePath("upstream/lib/src");
+    step.addIncludePath("upstream/lib/include");
 }
---
+++ src/tests.zig
@@ -0,0 +1,65 @@
+const std = @import("std");
+const testing = std.testing;
+const c = @import("c.zig").c;
+const Parser = @import("Parser.zig");
+
+// json parser copied from https://github.com/tree-sitter/tree-sitter-json/blob/40a81c01a40ac48744e0c8ccabbaba1920441199/src/parser.c
+extern fn tree_sitter_json() *anyopaque;
+
+// typescript parser copied from https://github.com/tree-sitter/tree-sitter-typescript/blob/3429d8c77d7a83e80032667f0642e6cb19d0c772/typescript/src/parser.c
+extern fn tree_sitter_typescript() *anyopaque;
+
+test "version" {
+    try testing.expectEqual(14, c.TREE_SITTER_LANGUAGE_VERSION);
+}
+
+// https://tree-sitter.github.io/tree-sitter/using-parsers#an-example-program
+test "an example program" {
+    const parser = Parser.new();
+    defer parser.delete();
+
+    const language = tree_sitter_json();
+    _ = parser.setLanguage(language);
+
+    const source_code = "[1, null]";
+    const tree = parser.parseString(source_code);
+    defer tree.delete();
+
+    const root_node = tree.getRoot();
+
+    const array_node = root_node.getNamedChild(0);
+    const number_node = array_node.getNamedChild(0);
+
+    try testing.expectEqualStrings("document", root_node.getType());
+    try testing.expectEqualStrings("array", array_node.getType());
+    try testing.expectEqualStrings("number", number_node.getType());
+
+    try testing.expectEqual(@as(u32, 1), root_node.getChildCount());
+    try testing.expectEqual(@as(u32, 5), array_node.getChildCount());
+    try testing.expectEqual(@as(u32, 2), array_node.getNamedChildCount());
+    try testing.expectEqual(@as(u32, 0), number_node.getChildCount());
+
+    const string = root_node.getString();
+    defer std.heap.raw_c_allocator.free(string);
+
+    try testing.expectEqualStrings("(document (array (number) (null)))", string);
+}
+
+test "typescript" {
+    const parser = Parser.new();
+    defer parser.delete();
+
+    const language = tree_sitter_typescript();
+    _ = parser.setLanguage(language);
+
+    const source_code = "function foo(a: string): number {}";
+    const tree = parser.parseString(source_code);
+    defer tree.delete();
+
+    const root_node = tree.getRoot();
+
+    const string = root_node.getString();
+    defer std.heap.raw_c_allocator.free(string);
+
+    try testing.expectEqualStrings("(program (function_declaration name: (identifier) parameters: (formal_parameters (required_parameter pattern: (identifier) type: (type_annotation (predefined_type)))) return_type: (type_annotation (predefined_type)) body: (statement_block)))", string);
+}
